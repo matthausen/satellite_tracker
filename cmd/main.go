577d302ad7a78192e1a6cef8b41a422c58df33f2
matthausen <matteo.fusilli@hotmail.it>
2021-03-06 21:32:23 +0000

    return json response

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"os"
 	"log"
 	"net/http"
+	"os"
 
 	model "github.com/matthausen/n2yo/src/model"
 )
 
 var (
 	baseURL = "https://api.n2yo.com/rest/v1/satellite/positions/25544/51.508/-0.125/0/2/&apiKey="
-	apiKey = os.Getenv("API_KEY")
+	apiKey  = os.Getenv("API_KEY")
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Hello")
+	w.Header().Set("Content-Type", "application/json")
+	satInfo := getSatInfo()
+	jsonData, err := json.Marshal(satInfo)
+	if err != nil {
+		log.Fatalf("Could not marhsla response: %v", err)
+	}
+	w.Write(jsonData)
 }
 
-func getSatInfo() {
-	resp, err := http.Get(baseURL+apiKey)
+func getSatInfo() model.Response {
+	resp, err := http.Get(baseURL + apiKey)
 	if err != nil {
 		log.Fatalf("Could not read from endpoint %v", err)
 	}
@@ -30,11 +35,11 @@ func getSatInfo() {
 		log.Println("Could not decode body:", err)
 	}
 
-	fmt.Println(satInfo)
+	return satInfo
 }
 
 func main() {
-	// http.HandleFunc("/", index)
+	http.HandleFunc("/", index)
 	getSatInfo()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
